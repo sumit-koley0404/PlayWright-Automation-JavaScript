8b125edd318466a85e353bcb1f7b72d237dc5962
sumit-koley0404 <sumitkoley180@gmail.com>
2025-02-02 10:46:50 +0530

    First Commit

---
+++ tests/login.spec.js
@@ -0,0 +1,24 @@
+const {test,expect}=require('@playwright/test');
+const {POManager}=require('../pageObject/POManager');
+const dataset=JSON.parse(JSON.stringify(require('../utils/login.json')));
+
+for(const credentials of dataset)
+{
+test(`@smoke valid LogIn for ${credentials.username}`,async({page})=>
+{
+    const textContent = page.locator("div[class='left mt-1'] h3");
+    const poManager = new POManager(page);
+    const loginPage = poManager.getLoginPage(); 
+    await loginPage.LogIn(credentials.username,credentials.password);
+    await expect(textContent).toHaveText("Automation");
+});
+test(`@smoke Invalid LogIn for ${credentials.username}`,async({page})=>
+{
+
+    const invalidLoginMessage =page.locator("div[aria-label='Incorrect email or password.']");
+    const poManager = new POManager(page);
+    const loginPage = poManager.getLoginPage();
+    loginPage.LogIn(credentials.username,credentials.password);
+    await expect(invalidLoginMessage).toHaveText("Incorrect email or password."); 
+})
+};
