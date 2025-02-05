b3e41efd1e574abe4dd666c065742952a5557810
sumit-koley0404 <sumitkoley180@gmail.com>
2025-02-05 22:38:27 +0530

    screenShot feature added on cofig file

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless : true
+    headless : true,
+    screenshot:'only-on-failure'
 
   },
 
