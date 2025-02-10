60b7292d6fc54a79b64cf5a00cc89e1be879836b
sumit-koley0404 <sumitkoley180@gmail.com>
2025-02-10 22:18:42 +0530

    updating page object with return txt method

--- pageObject/LogInPage.js
+++ pageObject/LogInPage.js
@@ -5,6 +5,9 @@ class LoginPage{
         this.userName = page.locator("#userEmail");
         this.passWord = page.locator("#userPassword");
         this.logInBtn = page.locator("#login");
+        this.validTextContent = page.locator("div[class='left mt-1'] h3");
+        this.invalidLoginContent =page.locator("div[aria-label='Incorrect email or password.']");
+
     }
     async LogIn(username,password)
     {
@@ -12,5 +15,14 @@ class LoginPage{
         await this.userName.fill(username);
         await this.passWord.fill(password);
         await this.logInBtn.click();
+        await this.page.waitForLoadState('networkidle');
+    }
+    async ValidTxtValidation()
+    {
+        return await this.validTextContent.textContent();
+    }
+    async InvalidTxtValidation()
+    {
+        return await this.invalidLoginContent.textContent();
     }
 }module.exports={LoginPage};
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,25 @@
 const {test,expect}=require('@playwright/test');
 const {POManager}=require('../pageObject/POManager');
+//const { LoginPage } = require('../pageObject/LogInPage');
 const dataset=JSON.parse(JSON.stringify(require('../utils/login.json')));
 
 for(const credentials of dataset)
 {
-test.only(`@smoke valid LogIn for ${credentials.username}`,async({page})=>
+test(`@smoke valid LogIn for ${credentials.username}`,async({page})=>
 {
-    const textContent = page.locator("div[class='left mt-1'] h3");
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage(); 
     await loginPage.LogIn(credentials.username,credentials.password);
-    await expect(textContent).toHaveText("Automation");
+    const txt = await loginPage.ValidTxtValidation();
+    expect(txt).toContain("Automation");
+    
 });
 test(`@smoke Invalid LogIn for ${credentials.username}`,async({page})=>
 {
-
-    const invalidLoginMessage =page.locator("div[aria-label='Incorrect email or password.']");
     const poManager = new POManager(page);
     const loginPage = poManager.getLoginPage();
-    loginPage.LogIn(credentials.username,credentials.password);
-    await expect(invalidLoginMessage).toHaveText("Incorrect email or password."); 
+    await loginPage.LogIn(credentials.username,credentials.password);
+    const txt = await loginPage.InvalidTxtValidation();
+    expect(txt).toContain("Incorrect email or password."); 
 })
 };
